e024a462b7ffbf0a81ecb2005ea39c11c5e42548
majdshahinISS <majd.shahin@iss-ag.com>
2025-09-05 17:03:47 +0200

    IPC_3APP test : 0 create and run 3 app in paraller
    Change-Id: I8095c7920305ff6ca718434a50634aed1def5306

---
+++ pkg/Dummy_Crypto_server/server/src/main.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdio.h>
+#include <unistd.h>
+#include <cstdio>
+#include <cstring>
+
+int main()
+{
+    std::cout << "Dummy_Crypto_server" << std::endl;
+    return 0;
+}
