e7fb1534ae01bad5461bca7ab5588ac383ba60f0
majdshahinISS <majd.shahin@iss-ag.com>
2025-09-08 09:42:10 +0200

    Revert "temp"
    This reverts commit 43329dc3f160005963ca34972b570f3264e02117.

--- pkg/Dummy_Crypto_client/server/src/main.cc
+++ pkg/Dummy_Crypto_client/server/src/main.cc
@@ -3,51 +3,9 @@
 #include <unistd.h>
 #include <cstdio>
 #include <cstring>
-#include <l4/sys/ipc.h>
-#include <l4/sys/utcb.h>
-#include <l4/re/env>
 
-#include <l4/util/util.h>
-#include "crypto_shared.h"
-
-
-static L4Re::Util::Registry_server<> server;
-
-class Calculation_server : public L4::Epiface_t<Calculation_server, Calc>
+int main()
 {
-public:
-  int op_sub(Calc::Rights, l4_uint32_t a, l4_uint32_t b, l4_uint32_t &res)
-  {
-    res = a - b;
+    std::cout << "Dummy_Crypto_client" << std::endl;
     return 0;
-  }
- 
-  int op_neg(Calc::Rights, l4_uint32_t a, l4_uint32_t &res)
-  {
-    res = -a;
-    return 0;
-  }
-};
- 
- 
-int
-main()
-{
-  static Calculation_server calc;
- 
-  // Register calculation server
-  if (!server.registry()->register_obj(&calc, "calc_server").is_valid())
-    {
-      printf("Could not register my service, is there a 'calc_server' in the caps table?\n");
-      return 1;
-    }
- 
-  printf("Welcome to the calculation server!\n"
-         "I can do subtractions and negations.\n");
- 
-  // Wait for client requests
-  server.loop();
- 
-  return 0;
 }
-
--- pkg/Dummy_Crypto_server/server/src/main.cc
+++ pkg/Dummy_Crypto_server/server/src/main.cc
@@ -1,50 +1,11 @@
-#include <l4/sys/ipc.h>
-#include <l4/sys/utcb.h>
-#include <l4/re/env>
 #include <iostream>
+#include <stdio.h>
+#include <unistd.h>
+#include <cstdio>
 #include <cstring>
 
-#include <l4/sys/err.h>
-#include <l4/sys/types.h>
-#include <l4/re/util/cap_alloc>
- 
-#include <stdio.h>
-
-#include "crypto_shared.h"
-
-int
-main()
+int main()
 {
-    L4::Cap<Calc> server = L4Re::Env::env()->get_cap<Calc>("calc_server");
-    if (!server.is_valid())
-    {
-        printf("Could not get server capability!\n");
-        return 1;
-    }
-    else
-    {
-        printf("Got server capability.\n");
-    }
-
-
-    l4_uint32_t val1 = 8;
-    l4_uint32_t val2 = 5;
-
-    printf("Asking for %d - %d\n", val1, val2);
-    if (server->sub(val1, val2, &val1))
-    {
-        printf("Error talking to server\n");
-        return 1;
-    }
-    printf("Result of subtract call: %d\n", val1);
-
-    printf("Asking for -%d\n", val1);
-    if (server->neg(val1, &val1))
-    {
-        printf("Error talking to server\n");
-        return 1;
-    }
-    printf("Result of negate call: %d\n", val1);
-
+    std::cout << "Dummy_Crypto_server" << std::endl;
     return 0;
 }
