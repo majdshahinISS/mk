030c333d1c4c0c618ef0204481b6bd70898cefcf
majdshahinISS <majd.shahin@iss-ag.com>
2025-09-08 11:48:33 +0200

    ipc works as calc server and client
    Change-Id: I0b43bf111edbe7c4ee70c9a54ebf8b5f70f17ad9

--- pkg/Dummy_Crypto_server/server/src/main.cc
+++ pkg/Dummy_Crypto_server/server/src/main.cc
@@ -4,8 +4,57 @@
 #include <cstdio>
 #include <cstring>
 
-int main()
+
+#include <l4/sys/ipc.h>
+#include <l4/sys/utcb.h>
+#include <l4/re/env>
+#include <l4/sys/err.h>
+#include <l4/sys/types.h>
+#include <l4/re/util/cap_alloc>
+ 
+
+#include <l4/re/util/object_registry>
+#include <l4/re/util/br_manager>
+#include <l4/sys/cxx/ipc_epiface>
+
+#include "/home/iss/L4Re_d/l4/pkg/user_shared/crypto_shared.h"
+
+
+static L4Re::Util::Registry_server<> server;
+ 
+class Calculation_server : public L4::Epiface_t<Calculation_server, Calc>
 {
-    std::cout << "Dummy_Crypto_server" << std::endl;
+public:
+  int op_sub(Calc::Rights, l4_uint32_t a, l4_uint32_t b, l4_uint32_t &res)
+  {
+    res = a - b;
     return 0;
+  }
+ 
+  int op_neg(Calc::Rights, l4_uint32_t a, l4_uint32_t &res)
+  {
+    res = -a;
+    return 0;
+  }
+};
+
+int
+main()
+{
+  static Calculation_server calc;
+ 
+  // Register calculation server
+  if (!server.registry()->register_obj(&calc, "calc_server").is_valid())
+    {
+      printf("Could not register my service, is there a 'calc_server' in the caps table?\n");
+      return 1;
+    }
+ 
+  printf("Welcome to the calculation server!\n"
+         "I can do subtractions and negations.\n");
+ 
+  // Wait for client requests
+  server.loop();
+ 
+  return 0;
 }
