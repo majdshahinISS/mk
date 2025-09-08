43329dc3f160005963ca34972b570f3264e02117
majdshahinISS <majd.shahin@iss-ag.com>
2025-09-08 09:42:01 +0200

    temp
    Change-Id: I72bf245cea90ac31c70fe6156f919e7f94850292

--- pkg/Dummy_Crypto_server/server/src/main.cc
+++ pkg/Dummy_Crypto_server/server/src/main.cc
@@ -1,11 +1,50 @@
+#include <l4/sys/ipc.h>
+#include <l4/sys/utcb.h>
+#include <l4/re/env>
 #include <iostream>
-#include <stdio.h>
-#include <unistd.h>
-#include <cstdio>
 #include <cstring>
 
-int main()
+#include <l4/sys/err.h>
+#include <l4/sys/types.h>
+#include <l4/re/util/cap_alloc>
+ 
+#include <stdio.h>
+
+#include "crypto_shared.h"
+
+int
+main()
 {
-    std::cout << "Dummy_Crypto_server" << std::endl;
+    L4::Cap<Calc> server = L4Re::Env::env()->get_cap<Calc>("calc_server");
+    if (!server.is_valid())
+    {
+        printf("Could not get server capability!\n");
+        return 1;
+    }
+    else
+    {
+        printf("Got server capability.\n");
+    }
+
+
+    l4_uint32_t val1 = 8;
+    l4_uint32_t val2 = 5;
+
+    printf("Asking for %d - %d\n", val1, val2);
+    if (server->sub(val1, val2, &val1))
+    {
+        printf("Error talking to server\n");
+        return 1;
+    }
+    printf("Result of subtract call: %d\n", val1);
+
+    printf("Asking for -%d\n", val1);
+    if (server->neg(val1, &val1))
+    {
+        printf("Error talking to server\n");
+        return 1;
+    }
+    printf("Result of negate call: %d\n", val1);
+
     return 0;
 }
